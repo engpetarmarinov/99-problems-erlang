2161fe75ffbfd0b71d786c5ea125215daa1740c9
Petar Marinov <eng.petar.marinov@gmail.com>
2019-06-19 00:33:53 +0300

    Update problem 7 desc

--- src/problem7.erl
+++ src/problem7.erl
@@ -2,8 +2,9 @@
 %%% @author petarmarinov
 %%% @copyright (C) 2019, <COMPANY>
 %%% @doc
-%%% Find out whether a list is a palindrome.
-%%% A palindrome can be read forward or backward; e.g. (x a m a x).
+%%% Flatten a nested list structure.
+%%% Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively).
+%%% Example: (my-flatten '(a (b (c d) e)))-> (A B C D E)
 %%% @end
 %%% Created : 18. Jun 2019 15:25
 %%%-------------------------------------------------------------------
